214c8b6aa61c64c226b02b621a81bc30342778c4
Perverseq <luchniknya@gmail.com>
2018-12-01 01:31:38 +0500

    Bugged version 0.2

--- X-O.py
+++ X-O.py
@@ -1,7 +1,5 @@
 import random
-import time
-GAME_FIELD = ['_1_', '_2_', '_3_', '_4_', '_5_', '_6_', '_7_', '_8_', '_9_']
-MOVES_LIST = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+#import time
 
 
 def print_field(game_field):
@@ -30,64 +28,68 @@ def choose_symbol():
 
 def move(game_field, player, comp, moves_list):
     print("Выберите, куда сходить")
+    #print_field(game_field)
     choosen_cell = int(input())
     try:
         game_field[choosen_cell - 1] = player
-        moves_list.pop(choosen_cell)
-        time.sleep(1)
-        comp_move = random.choice(moves_list)
-        game_field[comp_move - 1] = comp
-        moves_list.pop(comp_move)
+        moves_list.remove(choosen_cell)
+        #time.sleep(1)
+        if moves_list:
+            comp_move = random.choice(moves_list)
+            game_field[comp_move - 1] = comp
+            moves_list.remove(comp_move)
+        else:
+            pass
     except:
         print("Ход уже совершен. Выберите другое место.")
         move(game_field, player, comp, moves_list)
 
 
 def check_winner(game_field, game, player, moves_list):
-    for i in range(0, 7, 3):
-        if game_field[i] == game_field[i + 1] == game_field [i + 2]:
+    try:
+        for i in range(0, 6, 3):
+            if game_field[i] == game_field[i + 1] == game_field [i + 2]:
+                game = False
+                if game_field[i] == player:
+                    print("Вы победили!")
+                else:
+                    print("Вы проиграли!")
+        for i in range(3):
+            if game_field[i] == game_field[i + 3] == game_field[i + 6]:
+                game = False
+                if game_field[i] == player:
+                    print("Вы победили!")
+                else:
+                    print("Вы проиграли!")
+        if game_field[0] == game_field[4] == game_field[8]:
             game = False
-            if game_field[i] == player:
-                print("Вы победили!")
+            if game_field[0] == player:
+             print("Вы победили!")
+            else:
+             print("Вы проиграли!")
+        if game_field[2] == game_field[4] == game_field[6]:
+            game = False
+            if game_field[2] == player:
+             print("Вы победили!")
             else:
                 print("Вы проиграли!")
-
-    for i in range(3):
-        if game_field[i] == game_field[i + 3] == game_field[i + 6]:
+    except:
+        if not moves_list:
             game = False
-            if game_field[i] == player:
-                print("Вы победили!")
-            else:
-                print("Вы проиграли!")
-
-    if game_field[0] == game_field[4] == game_field[8]:
-        game = False
-        if game_field[0] == player:
-            print("Вы победили!")
-        else:
-            print("Вы проиграли!")
-
-    if game_field[2] == game_field[4] == game_field[6]:
-        game = False
-        if game_field[2] == player:
-            print("Вы победили!")
-        else:
-            print("Вы проиграли!")
-
-    if not moves_list:
-        game = False
-        print("Ничья!")
+            print("Ничья!")
     return game
 
 
 def main():
+    game_field = ['_1_', '_2_', '_3_', '_4_', '_5_', '_6_', '_7_', '_8_', '_9_']
+    moves_list = [0, 1, 2, 3, 4, 5, 6, 7, 8]
     player, comp = choose_symbol()
-    print_field(GAME_FIELD)
+    print_field(game_field)
     game = True
     while game:
-        move(GAME_FIELD, player, comp, MOVES_LIST)
-        print_field(GAME_FIELD)
-        game = check_winner(GAME_FIELD, game, player, MOVES_LIST)
+        move(game_field, player, comp, moves_list)
+        print_field(game_field)
+        game = check_winner(game_field, game, player, moves_list)
 
 
 if __name__ == '__main__':
